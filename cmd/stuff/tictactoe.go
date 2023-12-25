d09caa81281ce0029ceddf042f995b22b27e21ff
brandon-ohaver <brandon.ohaver@gmail.com>
2023-12-24 21:35:38 -0600

    Setup basic files for go project

---
+++ cmd/stuff/tictactoe.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello to my game of Tic-Tac-Toe!")
+
+	fmt.Println("  |   |  ")
+	fmt.Println("---------")
+	fmt.Println("  |   |  ")
+	fmt.Println("---------")
+	fmt.Println("  |   |  ")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module go_stuff
+
+go 1.21.5
